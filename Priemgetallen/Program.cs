d92a9fcd890b4e7c47973831348fb100e9103887
WimBervoetsPXL <wim.bervoets@pxl.be>
2026-04-27 21:09:35 +0200

    correcties

--- Priemgetallen/Program.cs
+++ Priemgetallen/Program.cs
@@ -14,28 +14,34 @@ namespace Priemgetallen
             //bool isValid = IsPrimeNumber(number);
             //Console.WriteLine(isValid ? $"{number} is een priemgetal." : $"{number} is geen priemgetal.");
 
+            // naamgeving is beter zo:
             int index = 0;
-            for (int i = 2; primeNumbers[99] == 0; i++)
+            for (int numberToCheck = 2; primeNumbers[99] == 0; numberToCheck++)
             {                
-                bool isPrimeNumber = IsPrimeNumber(i);
+                bool isPrimeNumber = IsPrimeNumber(numberToCheck);
                 if (isPrimeNumber)
                 {
-                    primeNumbers[index] = i;
-                    index += 1;
+                    primeNumbers[index] = numberToCheck;
+                    index++;
                 }
             }
 
             // lengte langste priemgetal bepalen om uitlijning te bepalen -> @Wim: is het de bedoeling dat ik hiermee iets doe? Want dat krijg ik niet op de wereld. 
-            int length = primeNumbers[99].ToString().Length;
-            
+            int length = primeNumbers[99].ToString().Length + 2;
+            // het laatste getal is per definitie ook hetgeen met het meeste cijfers, dus dit klopt inderdaad.
 
             Console.WriteLine("Dit zijn de eerste honderd priemgetallen: ");
             Console.WriteLine();
             
-           for (int i = 1; i <= primeNumbers.Length; i++)
+            //for (int i = 1; i <= primeNumbers.Length; i++)
+            for (int i = 0; i < primeNumbers.Length; i++) // Eerste index is ALTIJD 0
             {
-                Console.Write($"{primeNumbers[i-1], 5}"); // @Wim: ik vermoed dat ik die '5' hier moet vervangen door de lengte maar dat geeft errors.
-                if (i%10 == 0)
+                //Console.Write($"{primeNumbers[i], 5}"); // @Wim: ik vermoed dat ik die '5' hier moet vervangen door de lengte maar dat geeft errors.
+                // Klopt helemaal maar werkt gewoon niet met string interpolatie.
+                // die ", 5" is eigenlijk een verkorte notatie van PadLeft, dus dat is wat ik nu heb gedaan.
+                Console.Write($"{primeNumbers[i].ToString().PadLeft(length)}");
+                
+                if ((i + 1) %10 == 0) 
                 {
                     Console.WriteLine();
                 }
@@ -45,16 +51,14 @@ namespace Priemgetallen
 
         public static bool IsPrimeNumber(int number)
         {
-            if (number <= 1)
-                {
-                return false;
-                }
-
-            int modulus = 1;
+            //if (number <= 1)
+            //{
+            //    return false;
+            //}
             
             for (int i = 2; i <= (number / 2); i++)
             {
-                modulus = number % i;
+                int modulus = number % i;
                 if (modulus == 0)
                 {
                     return false;
